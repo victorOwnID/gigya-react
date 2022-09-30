0520228efa8a368fdebd7ed4e524273611f14e59
victorOwnID <victor@ownid.com>
2022-09-30 13:50:58 +0200

    Update LoginForm.jsx

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -29,7 +29,7 @@ function LoginForm() {
                 <input ref={passwordField} type="password" name="password" placeholder="password" required/>
                 <button type="submit">Log In</button>
                 <OwnID type='login'
-                       options={{ variant: 'button-fingerprint', infoTooltip:true, widgetPosition:'start', infoTooltipPosition:'bottom' }}
+                       options={{ variant: 'button-fingerprint', infoTooltip:true, widgetPosition:'start', infoTooltipPosition:'bottom', tooltip: { position: 'start' } }}
                        passwordField={passwordField}
                        loginIdField={emailField}
                        language='it'
